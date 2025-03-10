d98b6198452ab5f7f60cb915bbf71262a071daaf
Maikel Punie <maikel.punie@gmail.com>
2025-03-10 10:37:24 +0100

    more fixes for  vmb4LEDPWM

--- velbusaio/messages/channel_name_part2.py
+++ velbusaio/messages/channel_name_part2.py
@@ -84,6 +84,7 @@ class ChannelNamePart2Message(Message):
         "VMBGP4PIR-20",
         "VMB8DC-20",
         "VMB8IN-20",
+        "VMB4LEDPWM-20",
     ],
 )
 class ChannelNamePart2Message2(ChannelNamePart2Message):
--- velbusaio/messages/channel_name_part3.py
+++ velbusaio/messages/channel_name_part3.py
@@ -84,6 +84,7 @@ class ChannelNamePart3Message(Message):
         "VMBGP4PIR-20",
         "VMB8DC-20",
         "VMB8IN-20",
+        "VMB4LEDPWM-20",
     ],
 )
 class ChannelNamePart3Message2(ChannelNamePart3Message):
