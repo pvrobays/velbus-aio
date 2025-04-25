0d5c66689df2ac82382ff54f075b0dcfd1349732
Pieter-Jan Van Robays <pieterjan.vanrobays@gmail.com>
2025-04-25 21:31:31 +0200

    add VMBDALI and VMBDALI-20 to dimmer_status.py register

--- velbusaio/messages/dimmer_status.py
+++ velbusaio/messages/dimmer_status.py
@@ -25,7 +25,7 @@ LED_FAST_BLINKING = 1 << 5
 LED_VERY_FAST_BLINKING = 1 << 4
 
 
-@register(COMMAND_CODE, ["VMB1DM", "VMBDME", "VMB1LED"])
+@register(COMMAND_CODE, ["VMB1DM", "VMBDME", "VMB1LED", "VMBDALI", "VMBDALI-20"])
 class DimmerStatusMessage(Message):
     """
     sent by: VMBDME
